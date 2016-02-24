00bbdaeef10bb185230abb8e6a5c9c08d2f7683b
Tae-Hwa Lee <taihwa0130@gmail.com>
2016-02-24 16:00:28 +0900

    유니티 커밋 테스트
    유니티 커밋 테스트

---
+++ Assets/PlayMaker/Editor/PlayMakerAutoUpdater.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+namespace HutongGames.PlayMakerEditor
+{
+    /// <summary>
+    /// Try to fix common update problems automatically
+    /// This class does more in Unity 5
+    /// </summary>
+    [InitializeOnLoad]
+    public class PlayMakerAutoUpdater
+    {
+        const string version = "1.7.8.3";
+
+        // static constructor called on load
+        static PlayMakerAutoUpdater()
+        {
+            if (ShouldUpdate())
+            {
+                // Can't call assetdatabase here, so use update callback
+                EditorApplication.update -= RunAutoUpdate;
+                EditorApplication.update += RunAutoUpdate;
+            }
+        }
+
+        static bool ShouldUpdate()
+        {
+            if (string.IsNullOrEmpty(Application.dataPath)) return false;
+            return (EditorPrefs.GetString("PlayMaker.NoUpdateReminderForProject", "") != GetUpdateSignature());
+        }
+
+        // Get a unique signature for this update to avoid repeatedly updating the same project
+        // NOTE: might be a better way to do this. Currently doesn't catch project changes like imports...
+        static string GetUpdateSignature()
+        {
+            return Application.unityVersion + "__" + Application.dataPath + "__" + version;
+        }
+
+        public static bool IsPlayMakerUnity5VersionImported()
+        {
+            // not a foolproof test, but good enough...
+
+            return File.Exists(Application.dataPath+"/Plugins/PlayMaker/PlayMaker.dll");
+        }
+
+        public static void RunAutoUpdate()
+        {
+            //Debug.Log("PlayMaker AutoUpdater " + version);
+
+#if (UNITY_5_0 || UNITY_5)
+
+            if (!IsPlayMakerUnity5VersionImported())
+            {
+                if (!EditorUtility.DisplayDialog("PlayMaker",
+                    "Please update PlayMaker for Unity 5 compatibility." +
+                    "\nUpdate in the Unity 5 Asset Store, " +
+                    "or download from the Hutong Games store.",
+                    "OK", "Don't remind again"))
+                {
+                    EditorPrefs.SetString("PlayMaker.NoUpdateReminderForProject", GetUpdateSignature());
+                }
+            }
+#endif
+
+            EditorApplication.update -= RunAutoUpdate;
+        }
+    }
+}
---
+++ Assets/Script/RotateFruit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateFruit : MonoBehaviour {
+
+	public GameObject tomato;
+	public float Speed;
+	public float ForceRotate;
+
+	// Use this for initialization
+	void Start () {
+		//Speed = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0,0,Time.deltaTime * Speed);
+	}
+
+	void RotateZ () {
+		transform.Rotate = Quaternion.Euler (0, 0, ForceRotate);
+	}
+}
+ 
